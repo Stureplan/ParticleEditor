e74156907be0ed1c67f156f23f615a1dcc36a042
Stureplan <linus.ax@gmail.com>
2016-03-07 14:29:35 +0100

    Cleanup and renaming

--- Camera.cpp
+++ Camera.cpp
@@ -1,74 +1,74 @@
 #include "camera.h"
 
-void Camera::Initialize (glm::vec3 pos, glm::vec3 at, int width, int height)
+void Camera::Initialize(glm::vec3 pos, glm::vec3 at, int width, int height)
 {
 	View = glm::lookAt
-	(
-		pos,							//pos
-		at,								//at
-		glm::vec3 (0, 1, 0)				//up
-	);
+		(
+			pos,							//pos
+			at,								//at
+			glm::vec3(0, 1, 0)				//up
+			);
 
 	Projection = glm::perspective
-	(
-		glm::radians (45.0f),			//FOV
-		(float)width / (float)height,	//aspect ratio
-		0.1f,							//near
-		100.0f							//far
-	);
+		(
+			glm::radians(45.0f),			//FOV
+			(float)width / (float)height,	//aspect ratio
+			0.1f,							//near
+			100.0f							//far
+			);
 
 	Ortho = glm::ortho
 		(
-		0.0f,
-		(float)width,
-		(float)height,
-		0.0f
-	);
+			0.0f,
+			(float)width,
+			(float)height,
+			0.0f
+			);
 
 	m_position = pos;
 }
 
-void Camera::UpdateView ()
+void Camera::UpdateView()
 {
 	View = glm::lookAt
-	(
-		m_position,
-		m_direction,
-		m_up
-	);
+		(
+			m_position,
+			m_direction,
+			m_up
+			);
 }
 
-void Camera::SetPos (glm::vec3 pos)
+void Camera::SetPos(glm::vec3 pos)
 {
 	m_position = pos;
 }
 
-void Camera::SetDir (glm::vec3 dir)
+void Camera::SetDir(glm::vec3 dir)
 {
 	m_direction = dir;
 }
 
-void Camera::SetView (glm::mat4 v)
+void Camera::SetView(glm::mat4 v)
 {
 	View = v;
 }
 
-glm::vec3 Camera::GetPos ()
+glm::vec3 Camera::GetPos()
 {
 	return m_position;
 }
 
-glm::mat4 Camera::GetView ()
+glm::mat4 Camera::GetView()
 {
 	return View;
 }
 
-glm::mat4 Camera::GetProj ()
+glm::mat4 Camera::GetProj()
 {
 	return Projection;
 }
 
-glm::mat4 Camera::GetOrtho ()
+glm::mat4 Camera::GetOrtho()
 {
 	return Ortho;
 }
